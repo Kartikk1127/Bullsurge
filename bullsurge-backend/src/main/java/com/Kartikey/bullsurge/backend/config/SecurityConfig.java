87a05ba8f4654c1ae771ca0c37ff81fd04b7e8b5
Kartikey <kartikey.srivastava1271@gmail.com>
2023-05-24 04:49:01 +0530

    jwt implemented successfully

--- bullsurge-backend/src/main/java/com/Kartikey/bullsurge/backend/config/SecurityConfig.java
+++ bullsurge-backend/src/main/java/com/Kartikey/bullsurge/backend/config/SecurityConfig.java
@@ -1,21 +1,31 @@
 package com.Kartikey.bullsurge.backend.config;
 
+import com.Kartikey.bullsurge.backend.filter.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter authFilter;
 
     @Bean
     public UserDetailsService userDetailsService(){
@@ -26,10 +36,15 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/users", "/api/auth/add").permitAll()
+                .requestMatchers("/api/auth/users", "/api/auth/add", "/api/auth/authenticate").permitAll()
                 .and()
                 .authorizeHttpRequests().requestMatchers("/api/auth/**")
-                .authenticated().and().formLogin().and().build();
+                .authenticated().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class).build();
     }
 
     @Bean
@@ -43,5 +58,10 @@ public class SecurityConfig {
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- bullsurge-backend/src/main/java/com/Kartikey/bullsurge/backend/controller/UserController.java
+++ bullsurge-backend/src/main/java/com/Kartikey/bullsurge/backend/controller/UserController.java
@@ -3,11 +3,16 @@ package com.Kartikey.bullsurge.backend.controller;
 import com.Kartikey.bullsurge.backend.dto.AuthRequest;
 import com.Kartikey.bullsurge.backend.model.User;
 import com.Kartikey.bullsurge.backend.repository.UserRepository;
+import com.Kartikey.bullsurge.backend.service.JwtService;
 import com.Kartikey.bullsurge.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +31,12 @@ public class UserController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @GetMapping("/users")
     @PreAuthorize("hasRole('ADMIN')")
@@ -84,8 +95,14 @@ public class UserController {
 
     @PostMapping("/authenticate")
     public String authenticateAndGetToken(@RequestBody AuthRequest authRequest){
-
-
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if(authentication.isAuthenticated())
+        {
+            return  jwtService.generateToken(authRequest.getUsername());
+        }
+        else {
+            throw new UsernameNotFoundException("invalid username!");
+        }
     }
 
 
